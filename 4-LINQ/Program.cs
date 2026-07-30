abb8a33c9bce338f5eb7f88483e333f6e37eab3c
Krustu <kuban.ru86@gmail.com>
2026-07-30 21:30:19 +0600

    two more examples

--- 4-LINQ/Program.cs
+++ 4-LINQ/Program.cs
@@ -5,7 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("here LINQ with more or less");
+
+        Console.ReadKey();
+        Console.WriteLine("here LINQ with more or less (1,2,3,4 , 11, 12, 3, 34, 25, 36, 721, 38, 92, 10 ) ");
        List<int> numbers = new List<int> {1,2,3,4 , 11, 12, 3, 34, 25, 36, 721, 38, 92, 10 };
 
         var Result1 = numbers.Where(x => x > 5);
@@ -16,26 +18,27 @@ class Program
         }
 
 
-
-        Console.WriteLine("Even numbers ");
+        Console.ReadKey();
+        Console.WriteLine("Even numbers (1,2,3,4 , 11, 12, 3, 34, 25, 36, 721, 38, 92, 10 ) ");
 
           var Result2 = numbers.Where(x => x % 2 == 0);
           foreach (var b in Result2)
           {
               Console.WriteLine(b);
           }
-        Console.WriteLine(" Odd numbers");
+        Console.ReadKey();
+        Console.WriteLine(" Odd numbers(1,2,3,4 , 11, 12, 3, 34, 25, 36, 721, 38, 92, 10 ) ");
 
         var Result3 = numbers.Where(x => x % 2 != 0).ToList();
         for(int i = 0; i < Result3.Count; i++)
         {
             Console.WriteLine(Result3.ElementAt(i));
         }
-        
 
 
 
-        Console.WriteLine("lines more than 4 words");
+        Console.ReadKey();
+        Console.WriteLine("lines more than 4 words (\"hello world\", \"this is a test\", \"C# is great\", \"LINQ is powerful\", \"short\") ");
         var words = new List<string> { "hello world", "this is a test", "C# is great", "LINQ is powerful", "short" };
 
         var results4 = words.Where(w => w.Length > 4).ToList();
@@ -43,19 +46,66 @@ class Program
         {
             Console.WriteLine(l);
         }
-
-        Console.WriteLine("with for");
+        Console.ReadKey();
+        Console.WriteLine("with for (\"hello world\", \"this is a test\", \"C# is great\", \"LINQ is powerful\", \"short\") ");
         Console.ReadKey();
         for (int k = 0; k < results4.Count; k++)
         {
                         Console.WriteLine(results4.ElementAt(k));
         }
 
-        Console.WriteLine("object properties");
+        Console.ReadKey();
+        Console.WriteLine("object properties() ");
+        var peaple = new[]
+        {
+            new { Name = "Alice"  , Age = 30 },
+            new { Name = "Bob",     Age =  5 },
+            new { Name = "Charlie", Age = 35 },
+            new { Name = "Alice"  , Age = 10 },
+            new { Name = "Boba",    Age = 15 },
+            new { Name = "Char",    Age = 33 },
+            new { Name = "Alie"  ,  Age = 10 },
+            new { Name = "Bobobe",  Age = 22 },
+            new { Name = "Karlie",  Age = 5  }
+
+        };
+        var result5 = peaple.Where(X => X.Age > 10).ToList();
+        foreach (var p in result5)
+        {
+            Console.WriteLine(p);
+        }
 
 
 
-        Console.WriteLine("double conditions");
+
+
+
+
+        Console.ReadKey();
+        Console.WriteLine("double conditions() ");
+
+        var result6 = peaple.Where(x => x.Age >= 10 && x.Name.StartsWith("A")).ToList();
+        foreach (var o in result6)
+        {
+            Console.WriteLine(o);
+        } Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // for Arrays
 

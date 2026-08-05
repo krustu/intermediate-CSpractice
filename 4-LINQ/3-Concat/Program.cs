7cf9c309900980df27142384aa4e43d3b44b8612
Krustu <kuban.ru86@gmail.com>
2026-08-05 17:15:41 +0600

    a

--- 4-LINQ/3-Concat/Program.cs
+++ 4-LINQ/3-Concat/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+class AnyProgram
+{
+    static void Main()
+    {
+       
+        List<string> heroes = new()
+{
+    "Arthur",
+    "Merlin",
+    "Robin"
+};
+
+        List<string> monsters = new()
+{
+    "Goblin",
+    "Orc",
+    "Dragon",
+    "Dragon"
+};
+
+        var allHeroes = heroes.Concat(monsters).Distinct();
+        
+
+        foreach (var hero in allHeroes)
+        {
+            Console.WriteLine(hero);
+        }
+
+
+        Console.WriteLine("Number of Characters");
+        Console.WriteLine(allHeroes.Count());
+
+
+
+    }
+}
+/*int[] a = { 1, 2, 3, 4, 5, 6, 7, };
+        int[] b = { 8, 9, 10, 11, 12, 13, 14, 15, 1 , 2, 3, 4, 5, 6, 7, 8 };
+
+        var result = b.Concat(a).Distinct();
+
+        foreach (var i in result)
+        {
+            Console.WriteLine(i);
+        }*/
---
+++ 4-LINQ/4-Select/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+class SelectProgram
+{
+    static void Main()
+    {
+         var num = List<int>
+    }
+}

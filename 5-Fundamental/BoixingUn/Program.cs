759b8c1cc9ed2ad8c464a5a779347d19ed85221b
Krustu <kuban.ru86@gmail.com>
2026-08-20 21:51:34 +0600

    Boxing Unboxing

---
+++ 5-Fundamental/BoixingUn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int Number = 10;
+        object RefNumber = Number;
+        Number += 10;
+        Number = (int)RefNumber;
+        Number += 10;
+        Console.WriteLine(Number);
+        Console.WriteLine(RefNumber);
+
+
+
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}

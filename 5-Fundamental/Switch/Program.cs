50b6f9feae58b75ddc7bdfb4f82490feb7b07a67
Krustu <kuban.ru86@gmail.com>
2026-08-20 22:25:04 +0600

    switch else if examples show Difference

---
+++ 5-Fundamental/Switch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("With using If else");
+        int score = 0;
+        string result;
+        if ( score >= 80)
+        {
+            result = "excellent";
+        }
+        else if( score >= 60)
+        {
+            result = "Good ";
+        }
+        else if (score >= 40)
+        {
+            result = "Not bad";
+        }
+        else
+        {
+            result = "Dumb";
+   
+        }
+
+        Console.WriteLine($"You are - {result}");
+        Console.ReadKey();
+
+        Console.WriteLine("With using Switch");
+        string grade = score switch
+        {
+            >= 80 => "God",
+            >= 60 => "good",
+            >= 40 => "Norm",
+            _ => "Idiot"
+        };
+        Console.WriteLine($"You are - {grade}");
+        Console.ReadKey();
+    }
+}

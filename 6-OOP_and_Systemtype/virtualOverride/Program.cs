10e8fc3fa09c66a88fb43b23e94f85dfa98ce857
Krustu <kuban.ru86@gmail.com>
2026-08-25 04:22:47 +0600

    aa

--- 6-OOP_and_Systemtype/virtualOverride/Program.cs
+++ 6-OOP_and_Systemtype/virtualOverride/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using System;
+class Program
+{
+    static void Main()
+    {
+        Derived d = new Derived();
+        Base b = d; // тот же объект, но переменная типа Base
+
+        d.Greet();
+        d.Hello();
+        b.Greet();
+        b.Hello();
+    }
+}
+public class Base
+{
+    public virtual void Greet() => Console.WriteLine("Base greet");
+    public void Hello() => Console.WriteLine("Base hello"); // без virtual
+}
+
+public class Derived : Base
+{
+    public override void Greet() => Console.WriteLine("Derived greet");
+    public new void Hello() => Console.WriteLine("Derived hello");
+}

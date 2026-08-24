be6ea5c8c8c13997d8dec841e12eb6331d15c4c6
Krustu <kuban.ru86@gmail.com>
2026-08-25 03:15:30 +0600

    private public proporties that can take mistake and return the message

---
+++ 6-OOP_and_Systemtype/Encapsulation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Person person = new Person(0, "");
+
+            Console.WriteLine($"Name - {person.Name} Age - {person.Age}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        Console.ReadKey();
+
+    }
+}
+public class Person
+{
+    private int _age;
+    public int Age
+    {
+        get
+        {
+            return _age;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Bro write your age correctly!");
+            }
+            _age = value;
+        }
+    }
+    private string _name;
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            if (value == null || value == "")
+            {
+                throw new ArgumentException("Write Your Name!");
+            }
+            else
+            {
+                _name = value;
+            }
+
+        }
+    }
+    public Person(int age, string name)
+    {
+        Age = age;
+        Name = name;
+    }
+}
+
+

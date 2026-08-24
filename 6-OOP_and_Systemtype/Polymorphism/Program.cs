0cad9f086bba003570bcb92ff7e66a4979fd2056
Krustu <kuban.ru86@gmail.com>
2026-08-25 02:32:34 +0600

    small mini task show as how to work polymorphism with interface and independent function that can work with another LIst etc

---
+++ 6-OOP_and_Systemtype/Polymorphism/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+class Program
+{
+    static void Main()
+    {
+        List<INotifier> notifiers = new List<INotifier>
+        {
+            new EmailNotifier(),
+            new SmsNotifier(),
+            new PushNotifier()
+        };
+
+        NotifyAll(notifiers, "poshel nahyi");
+    }
+    static void NotifyAll(List<INotifier> notifiers, string message)
+    {
+        foreach (INotifier a in notifiers)
+        {
+            a.Send(message);
+        }
+    }
+}
+public interface INotifier
+{
+    void Send(string message);
+}
+public class EmailNotifier : INotifier
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Email sent :{message}");
+    }
+}
+
+public class SmsNotifier : INotifier
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"User sent :{message}");
+    }
+}
+public class PushNotifier : INotifier
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Bro sent :{message}");
+    }
+}

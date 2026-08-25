280f5b10c4e3a21df361d37318c700ff56cb42b9
Krustu <kuban.ru86@gmail.com>
2026-08-25 22:52:53 +0600

    aa

---
+++ 6-OOP_and_Systemtype/Init/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+
+        Order order = new Order { ProductName = "Laptop", Price = 1000, Quantity = 3 };
+
+        Console.WriteLine(order.Total);
+
+
+        order.Price = 1500;
+        Console.WriteLine(order.Total);
+        Console.ReadKey();
+
+        /*
+     
+Выведи order.Total
+Попробуй специально написать order.ProductName = "Другое название"; после создания — убедись, что компилятор выдаст ошибку из-за init, покажи мне текст ошибки
+Измени order.Price на другое значение и снова выведи order.Total — убедись, что число пересчиталось само, без дополнительного кода с твоей стороны
+
+Смысл — почувствовать разницу между "хранимым" значением (Price, можно менять) и "вычисляемым" (Total, всегда актуально автоматически).*/
+    }
+}
+public class Order
+{
+    public string ProductName { get; init; } // задаётся один раз при создании
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public decimal Total => Price * Quantity; // вычисляемое свойство
+}
--- 6-OOP_and_Systemtype/ProtectedLevel/Program.cs
+++ 6-OOP_and_Systemtype/ProtectedLevel/Program.cs
@@ -1,1 +1,65 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        Manager managaer = new Manager(25530m, "Rysbek");
+        managaer.GetInfo();
+        managaer.AwardBonus();
+        managaer.GetInfo();
+      //  managaer.CalculateBonus(); we cannot use method from main class now is protected for safety. we can only work with inherited class -Manger
+    }
+}
+public class Employee
+{
+    private string Name { get; set; }
+    private decimal _salary;
+    public decimal Salary
+    {
+        get
+        {
+            return _salary;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Cannot be negative");
+            if (value > 1_000_000)
+                throw new ArgumentException("Salary is too high");
+            _salary = value;
+        }
+    }
+    public Employee(decimal salary, string name)
+    {
+        Salary = salary;
+        Name = name;
+    }
+    protected decimal CalculateBonus()
+    {
+
+        return Salary * 0.1m;
+
+
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine($"Name- {Name} Salary-{Salary} Usd");
+    }
+
+}
+
+public class Manager : Employee
+{
+    public Manager(decimal salary, string name) : base(salary, name) { }
+    public void AwardBonus()
+    {
+
+        var result = CalculateBonus();
+
+        Console.WriteLine($"Bonus - {result} Usd");
+    }
+
+}
+
+

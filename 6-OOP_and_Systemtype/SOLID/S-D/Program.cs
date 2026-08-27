f56d93821c2b83ba7389d61f2ed7b8785fbedb0c
Krustu <kuban.ru86@gmail.com>
2026-08-27 22:38:41 +0600

    Finished first pracice of SOLID

---
+++ 6-OOP_and_Systemtype/SOLID/S-D/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+class Pragram
+{
+    static void Main()
+    {
+        Console.WriteLine();
+        OrderCalculator calculator = new OrderCalculator();
+        OrderRepository repository = new OrderRepository();
+        EmailSender emailSender = new EmailSender();
+        var res = new OrderProcessor(emailSender, calculator, repository);
+
+        res.ProcessOrder("Fanta", 14.3m);
+        Console.ReadKey();
+    }
+}
+public interface INotificationSender
+{
+    void Send(string message); //=> Console.WriteLine($"[Email] {message}");
+}
+
+public class OrderProcessor
+{
+    private INotificationSender _sender;
+    private OrderCalculator _calculator;
+    private OrderRepository _repository;
+    public OrderProcessor(INotificationSender sender, OrderCalculator calculator, OrderRepository repository)
+    {
+        _sender = sender;
+        _calculator = calculator;
+        _repository = repository;
+    }
+    public void ProcessOrder(string productName, decimal price)
+    {
+        decimal total = _calculator.TakeTax(price);
+        _repository.SaveOrder(productName, total);
+        _sender.Send($"Order {productName} for this amount - {total} usd - has been placed");
+    }
+}
+
+
+public class OrderCalculator
+{
+    private decimal Tax = 1.2m;
+    public decimal TakeTax(decimal amount)
+    {
+        decimal total = amount * Tax;
+        return total;
+    }
+
+}
+public class OrderRepository
+{
+    public void SaveOrder(string productName, decimal price)
+    {
+        Console.WriteLine($"[DB] Saving order: {productName} - {price}");
+    }
+
+}
+public class EmailSender : INotificationSender
+{
+    public void Send(string email) => Console.WriteLine($"[Email] {email}");
+}
+
+public class SmsSender : INotificationSender
+{
+    public void Send(string sms) => Console.WriteLine($"[User] {sms}");
+}
+
+
+

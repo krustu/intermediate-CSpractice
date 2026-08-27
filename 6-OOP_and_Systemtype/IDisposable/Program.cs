247b7594438a8d0aa0d69d974e564495eeace57e
Krustu <kuban.ru86@gmail.com>
2026-08-27 19:33:47 +0600

    added one task more with using disposable interface that can work even program get crush

---
+++ 6-OOP_and_Systemtype/IDisposable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        using (var logger = new FileLogger("app.log"))
+        {
+
+
+            logger.Log("smt");
+
+        }
+
+
+        Console.ReadKey();
+
+        using (FileLogger logger2 = new FileLogger("app.log"))
+        {
+            logger2.Log("hello everyone");
+        }
+
+
+
+
+
+
+    }
+}
+
+public class FileLogger : IDisposable
+{
+    private string _fileName;
+
+    public FileLogger(string fileName)
+    {
+        _fileName = fileName;
+        Console.WriteLine($"file {_fileName} open");
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"[{_fileName}] {message}");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"File {_fileName} closed");
+    }
+}

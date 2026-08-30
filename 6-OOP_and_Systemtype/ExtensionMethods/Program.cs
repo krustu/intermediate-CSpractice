89907e8656a3331e1196d964a2f9e182e1aa0b5b
Krustu <kuban.ru86@gmail.com>
2026-08-30 20:04:02 +0600

    small example with extension method

--- 6-OOP_and_Systemtype/ExtensionMethods/Program.cs
+++ 6-OOP_and_Systemtype/ExtensionMethods/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string Fullname = "Rysbek Dokturbaev";
+        var Result = Fullname.Truncut(10);
+
+
+        Console.ReadKey();
+
+    }
+}
+public static class StringBuilder
+{
+    public static string Truncut(this string amo, int max)
+    {
+        if (amo.Length <= max)
+            return amo;
+        return amo.Substring(0, max);
+    }
+}

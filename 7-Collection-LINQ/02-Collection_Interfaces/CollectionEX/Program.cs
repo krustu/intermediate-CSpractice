9a5a4740fb9199559f714b25d240036ec54086e2
Krustu <kuban.ru86@gmail.com>
2026-09-02 23:25:58 +0600

    a

---
+++ 7-Collection-LINQ/02-Collection_Interfaces/CollectionEX/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Book
+{
+    public string Title { get; set; }
+
+    public Book(string title)
+    {
+        Title = title;
+    }
+
+    public override string ToString()
+    {
+        return Title;
+    }
+}
+
+class Library
+{
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    // 1. IEnumerable<T>
+    public IEnumerable<Book> GetBooks()
+    {
+        return books;
+    }
+
+    // 2. ICollection<T>
+    public ICollection<Book> GetBookCollection()
+    {
+        return books;
+    }
+
+    // 3. IList<T>
+    public IList<Book> GetBookList()
+    {
+        return books;
+    }
+
+    // 4. IReadOnlyList<T>
+    public IReadOnlyList<Book> GetReadOnlyBooks()
+    {
+        return books;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Library library = new Library();
+
+        library.AddBook(new Book("Harry Potter"));
+        library.AddBook(new Book("The Hobbit"));
+        library.AddBook(new Book("1984"));
+    }
+}
---
+++ 7-Collection-LINQ/02-Collection_Interfaces/Hierarchy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ShopingCart = new ShoppingCart();
+        ShopingCart.AddItem("Item 1");
+        ShopingCart.AddItem("Item 2");
+        ShopingCart.AddItem("Item 3");
+
+
+        var itemCount = ShopingCart.CountItems(ShopingCart.GetItems());
+        var firstItem = ShopingCart.GetFirst(ShopingCart.GetItems());
+
+        Console.WriteLine($"Total items: {itemCount}");
+        Console.WriteLine($"First item: {firstItem}");
+        var allItems = ShopingCart.GetItems();
+        Console.WriteLine("All items:");
+        ShopingCart.PrintAll(allItems);
+        Console.ReadKey();
+    }
+
+}
+public class ShoppingCart
+{
+    private List<string> _items = new List<string>();
+
+    public void AddItem(string item)
+    {
+        _items.Add(item);
+    }
+
+
+    public IReadOnlyList<string> GetItems() => _items;
+
+
+    public void PrintAll(IEnumerable<string> list)
+    {
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    public int CountItems(IReadOnlyList<string> list)
+    {
+
+        return list.Count;
+    }
+    public string GetFirst(IReadOnlyList<string> list)
+    {
+        return list[0];
+    }
+}

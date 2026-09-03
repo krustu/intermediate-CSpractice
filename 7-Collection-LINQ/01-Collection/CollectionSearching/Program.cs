d7a4d876ad227db94f08b107326aba22fcf70909
Krustu <kuban.ru86@gmail.com>
2026-09-03 18:29:40 +0600

    added small example that shows as process of working Stopwatch and different velocity time between list and hashset

---
+++ 7-Collection-LINQ/01-Collection/CollectionSearching/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        var list = new List<int>();
+        var hashSet = new HashSet<int>();
+
+        for (int i = 0; i < 100000; i++)
+        {
+            list.Add(i);
+            hashSet.Add(i);
+        }
+
+
+        var a = Stopwatch.StartNew();
+        bool find1 = list.Contains(99999);
+        a.Stop();
+        Console.WriteLine($"List.Contains: {a.ElapsedTicks} ticks");
+
+        var b = Stopwatch.StartNew();
+        bool find2 = hashSet.Contains(99999);
+        b.Stop();
+        Console.WriteLine($"HashSet.Contains: {b.ElapsedTicks} ticks");
+
+
+
+    }
+
+}

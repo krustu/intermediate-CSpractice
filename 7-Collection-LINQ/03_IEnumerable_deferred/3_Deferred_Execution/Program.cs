8a3bacaec85dff991142f67720260dd0d0713ee5
Krustu <kuban.ru86@gmail.com>
2026-09-04 22:27:28 +0600

    added another example that demonstrate in detail lazy sequence

---
+++ 7-Collection-LINQ/03_IEnumerable_deferred/3_Deferred_Execution/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        var query = numbers.Where(n =>
+        {
+            Console.WriteLine($"Checking: {n}");
+            return n % 2 == 0;
+        }).ToList(); // Materialize the query
+                     //filtring happens immediately because of ToList()
+
+        /* Console.WriteLine("Second call through foreach:");
+         foreach (var n in query)
+         {
+             Console.WriteLine($"Received: {n}");
+         } */
+
+
+        Console.WriteLine("First call to Count():");
+        int count = query.Count();
+        Console.WriteLine($"Result: {count}");
+
+        Console.WriteLine("Second call through foreach:");
+        foreach (var n in query)
+        {
+            Console.WriteLine($"Received: {n}");
+        }
+    }
+
+
+}

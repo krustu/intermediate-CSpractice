4680925eebb9bec27be86a702c24cbd6e06b9e81
Krustu <kuban.ru86@gmail.com>
2026-09-04 21:21:19 +0600

    add method for see how works Enumerable lazy sequance

---
+++ 7-Collection-LINQ/02-Collection_Interfaces/IEnumerableSequence/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        foreach (var num in EvenNumbersUpTo(10))
+        {
+            Console.WriteLine($"Taken: {num}");
+            if (num == 10) break;
+        }
+    }
+    public static IEnumerable<int> EvenNumbersUpTo(int max)
+    {
+        for (int a = 0; a < max; a += 1)
+        {
+            Console.WriteLine($"Generated: {a}");
+            yield return a;
+
+        }
+    }
+
+
+}

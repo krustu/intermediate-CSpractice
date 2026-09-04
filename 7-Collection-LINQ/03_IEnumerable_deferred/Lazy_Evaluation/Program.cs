53ba7440b6a0d4ab5a20ad638e5b6ac97637c2e5
Krustu <kuban.ru86@gmail.com>
2026-09-04 22:13:11 +0600

    explore and test another ex with lzay sequence

---
+++ 7-Collection-LINQ/03_IEnumerable_deferred/Lazy_Evaluation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        List<string> names = new List<string> { "Alice", "Bob" };
+        var query = names.Where(n => n.StartsWith("A"));
+
+        names.Add("Anna"); // добавили ПОСЛЕ создания query
+
+        Console.WriteLine("Первый перебор:");
+        foreach (var n in query)
+        {
+            Console.WriteLine(n);
+        }
+
+        names.Remove("Alice"); // удалили ПОСЛЕ первого перебора
+
+        Console.WriteLine("Второй перебор:");
+        foreach (var n in query)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}

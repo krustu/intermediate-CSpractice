f9419520328df22e48eb2cece3f765700809a59f
Krustu <kuban.ru86@gmail.com>
2026-09-08 01:34:32 +0600

    added just short file with discription about functions of LINQ

---
+++ 7-Collection-LINQ/04_Basic_LINQ/First_Single-OrDefault/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 5, 1, 3, 1 };
+        // 5
+
+
+        int firstEven = numbers.FirstOrDefault(n => n % 2 == 0);
+        Console.WriteLine(firstEven);
+        // single when item is unique in otherwise it will throw an exception "first" cannot throw an exception if the item is not found, it will return the default value for the type (in this case, 0 for int)
+        // first is the same as firstordefault but it will throw an exception if the item is not found
+
+        try
+        {
+            int singleEven = numbers.Single(n => n % 2 == 0);
+            Console.WriteLine(singleEven);
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+}

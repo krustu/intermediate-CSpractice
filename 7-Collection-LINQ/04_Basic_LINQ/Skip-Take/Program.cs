254e026b18790a6bb8fb59c923c53a9f31ed81c4
Krustu <kuban.ru86@gmail.com>
2026-09-09 18:43:22 +0600

    add GetPAge method examples

---
+++ 7-Collection-LINQ/04_Basic_LINQ/Skip-Take/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main()
+    {
+        List<string> names = new List<string>
+{
+    "Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Henry", "Ivy", "Jack"
+};
+        // 1Collection 2 Page 3 Size   
+        var page1 = GetPage(names, 2, 5);
+        foreach (var name in page1)
+        {
+            Console.WriteLine(name);
+        }
+
+
+    }
+    static List<string> GetPage(List<string> Items, int PageNum, int PageSize)
+    {
+        return Items.Skip((PageNum - 1) * PageSize).Take(PageSize).ToList();
+    }
+}
+
+

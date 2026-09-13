cf8337051bc47914c34b8663bf7e005490b3b922
Krustu <kuban.ru86@gmail.com>
2026-09-13 20:53:24 +0600

    add full structure of Exception including try,catch,finaly with using OOP structure

---
+++ 8-Exceptions/01_Exception_Basics/Exception_oop_Finally/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var Num = DataNames.GetDataNames.Count();
+        FileProcessor processor = new FileProcessor();
+        processor.Open();
+        for (int a = 0; a < Num; a++)
+        {
+            processor.ReadByNum(DataNames.GetDataNames, a);
+        }
+
+    }
+}
+public class People
+{
+    public required int Id { get; set; }
+    public string? Name { get; set; }
+}
+class FileProcessor
+{
+    public void Open()
+    {
+        Console.WriteLine("Program Opening..");
+    }
+    public string ReadByNum(List<People> list, int index)
+    {
+        try
+        {
+            People person = list[index];
+
+            if (person.Name == null)
+            {
+                throw new ArgumentNullException(nameof(person.Name));
+            }
+            else if (person.Name == "")
+                throw new ArgumentException(
+                    "PErson name cannot be empty", nameof(person.Name));
+
+            Console.WriteLine($"{person.Name} was added successfuly");
+            return person.Name;
+
+
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return "no name";
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            return "Empty name";
+        }
+        finally
+        {
+
+            Console.WriteLine("Program Closing...");
+        }
+    }
+}
+public static class DataNames
+{
+    public static List<People> GetDataNames { get; } = new List<People> {
+        new People {Id = 1 , Name = null},
+        new People {Id = 2 , Name = "KRustu"},
+        new People {Id = 3 , Name = "John"},
+        new People {Id = 4 , Name = null},
+        new People {Id = 5 , Name = "Salovey"},
+        new People {Id = 6 , Name = "Indian"},
+        new People {Id = 7 , Name = "Robot22"},
+        new People {Id = 8 , Name = ""},
+    };
+}
--- 8-Exceptions/01_Exception_Basics/README.md
+++ 8-Exceptions/01_Exception_Basics/README.md
@@ -12,3 +12,10 @@ Exception
 │   └── KeyNotFoundException
 └── ApplicationException (устаревший, для своих исключений — на практике почти не используют, чаще наследуют прямо от Exception)
 
+## Exception Hierarchy
+
+This diagram shows the concept of how the exception hierarchy works.
+
+Examples and practice can be found in the first file, **`Exception-Hierarchy`**.
+
+

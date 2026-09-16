1e472d4f8ea3d3905455e38c1dfc8aecacbb8866
Krustu <kuban.ru86@gmail.com>
2026-09-16 20:05:52 +0600

    comparing two fucntion speeds , first with using If Condition second with Exception and whe excaption can be worst and bad idea

---
+++ 8-Exceptions/05_Exceptions_And_Resources/Exception-CheckWithStopWatch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main()
+    {
+
+        CheckWithIf(-1);
+        CheckWithException(-1);
+
+        var a = Stopwatch.StartNew();
+        for (int i = -10000; i < 1; i++)
+        {
+            CheckWithIf(i);
+        }
+        a.Stop();
+        Console.WriteLine($"CheckWithIf - {a.ElapsedTicks}");
+
+        a.Restart();
+        for (int i = -10000; i < 1; i++)
+        {
+            CheckWithException(i);
+        }
+        a.Stop();
+        Console.WriteLine($"CheckWithException - {a.ElapsedTicks}");
+    }
+    public static bool CheckWithIf(int n)
+    {
+        if (n < 0)
+        {
+            return false;
+        }
+        return true;
+    }
+    public static bool CheckWithException(int n)
+    {
+        try
+        {
+            if (n < 0)
+                throw new ArgumentException();
+            return true;
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+    }
+}

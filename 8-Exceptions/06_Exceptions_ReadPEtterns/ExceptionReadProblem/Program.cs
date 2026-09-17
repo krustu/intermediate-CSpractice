b47b84a0efcf41cef1d4af900bd8b0ba8bba8723
Krustu <kuban.ru86@gmail.com>
2026-09-17 18:52:15 +0600

    add Investigation a method for identifying problems using Exception

---
+++ 8-Exceptions/06_Exceptions_ReadPEtterns/ExceptionReadProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main()
+    {
+        Order a = new Order();
+        a.PrintFirstItem();
+        Console.ReadKey();
+        StackTrace ResultMistake = new StackTrace();
+        Console.WriteLine(ResultMistake);
+    }
+}
+public class Order
+{
+    List<string> Item = new();
+    public void PrintFirstItem()
+    {
+        try
+        {
+            if (Item != null && Item.Count > 0)
+                Console.WriteLine(Item[0]);
+
+            else
+            {
+                //  string item = Item?[0] ?? "Nothing";
+                Console.WriteLine("Nothing");
+
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+
+        }
+    }
+}

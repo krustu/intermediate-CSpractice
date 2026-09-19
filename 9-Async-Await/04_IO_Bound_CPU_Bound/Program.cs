73f9db23fa96739ac361db31222585bbf31584db
Krustu <kuban.ru86@gmail.com>
2026-09-19 19:15:35 +0600

    compering time two function acync and simple version both of them has the same result

---
+++ 9-Async-Await/04_IO_Bound_CPU_Bound/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+class Program
+{
+    static async Task Main()
+    {
+        var sw1 = Stopwatch.StartNew();
+        CpuBreakerWork();
+        sw1.Stop();
+        Console.WriteLine($"Sync: {sw1.ElapsedMilliseconds} ms");
+
+        var sw2 = Stopwatch.StartNew();
+        await CpuBreakerWorkAsync();
+        sw2.Stop();
+        Console.WriteLine($"Async: {sw2.ElapsedMilliseconds} ms");
+    }
+    static void CpuBreakerWork()
+    {
+
+        //  await Task.Delay(3000);
+
+        for (int a = 1; a < 100_000_000_0; a++)
+        {
+            if (a == 100_000_000_0)
+            {
+                Console.WriteLine("Finish");
+            }
+        }
+    }
+    static async Task CpuBreakerWorkAsync()
+    {
+        await Task.Run(() => CpuBreakerWork());
+    }
+}
+

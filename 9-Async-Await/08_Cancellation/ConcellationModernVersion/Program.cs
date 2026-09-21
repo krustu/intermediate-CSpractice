778e69b59dac421fa6deed1aa1b842d7186da3c3
Krustu <kuban.ru86@gmail.com>
2026-09-21 18:33:24 +0600

    add first example with practice concellation usingby modern version

--- 9-Async-Await/08_Cancellation/ConcellationModernVersion/Program.cs
+++ 9-Async-Await/08_Cancellation/ConcellationModernVersion/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+class Program
+{
+    static async Task Main()
+    {
+        var Cts = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+        try
+        {
+            var checkforans = await CountToTenAsync(Cts.Token);
+
+            Console.WriteLine(checkforans);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Operationg has been canceled");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Closing...");
+        }
+    }
+    static async Task<string> CountToTenAsync(CancellationToken token)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            token.ThrowIfCancellationRequested();
+            Console.WriteLine($"step - {i}");
+            await Task.Delay(500, token);
+
+        }
+        return "Finish";
+    }
+}

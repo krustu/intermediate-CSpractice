0b18e477dd93cafdeb2fa1eced4cb4e29b51f8dc
Krustu <kuban.ru86@gmail.com>
2026-09-22 18:14:00 +0600

    add example when  Concellation can be implement with stupid way. several certain way how tow write async function with correct cancellation and incorrect

--- 9-Async-Await/09_CancellationExceptions/Program.cs
+++ 9-Async-Await/09_CancellationExceptions/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+class Program
+{
+    static async Task Main()
+    {
+        var cts = new CancellationTokenSource();
+        //TimeSpan.FromSeconds(2)
+        var a = GoodWorkerAsync(cts.Token);
+
+        await Task.Delay(2000); // adjustment time 
+
+        cts.Cancel();
+        Console.WriteLine("Operation has been closed");
+
+        //Incorrect version
+        await Task.Delay(1000);
+
+        var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+        await BadWorkerAsync(cts2.Token);
+
+
+    }
+    static async Task GoodWorkerAsync(CancellationToken token)
+    {
+        for (int a = 0; a < 20; a++)
+        {
+            token.ThrowIfCancellationRequested();
+            Console.WriteLine($"step - {a}");
+            await Task.Delay(300, token);
+
+        }
+    }
+    static async Task BadWorkerAsync(CancellationToken token)
+    {
+        for (int a = 0; a < 20; a++)
+        {
+            //without writing - title.ThrowIfCancellationRequested();
+            Console.WriteLine($"step - {a}");
+            await Task.Delay(300);//, token)
+
+        }
+        Console.WriteLine("Program successfuly has done without any stopping");
+    }
+
+
+}

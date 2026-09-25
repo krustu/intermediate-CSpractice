151a93dd36fb0d82be29ba9da3de3a485a75d10c
Krustu <kuban.ru86@gmail.com>
2026-09-25 18:01:49 +0600

    add async void examplewhy it is bad

---
+++ 9-Async-Await/12_AsyncVoid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static async Task Main()
+    {
+        try
+        {
+            await GoodMethod();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            BadMethod();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    static async Task GoodMethod()
+    {
+        await Task.Delay(500);
+        throw new Exception("From task");
+    }
+    static async void BadMethod()
+    {
+        await Task.Delay(500);
+        throw new Exception("From void");
+    }
+}

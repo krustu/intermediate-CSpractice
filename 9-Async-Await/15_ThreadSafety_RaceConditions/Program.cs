dd69286ea2080364de44e70dd6efad4b203055c2
Krustu <kuban.ru86@gmail.com>
2026-09-26 18:13:06 +0600

    add ThreadSafety example. In this example we use correct collection for async function . and respond why simple collection list hashset can be dangurous in streams

---
+++ 9-Async-Await/15_ThreadSafety_RaceConditions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+class Program
+{
+    static async Task Main()
+    {
+        //   var numbers = new ConcurrentBag<int>();
+        var numbers = new List<int>();
+        //second method with Lock
+        var resulTasks = new List<Task>();
+        var lockobject = new object();
+
+        for (int a = 0; a < 1000; a++)
+        {
+
+            int captured = a;
+
+            resulTasks.Add(Task.Run(() =>
+            {
+                lock (lockobject)
+                {
+                    numbers.Add(captured);
+                }
+
+
+            }));
+        }
+        await Task.WhenAll(resulTasks);
+        Console.WriteLine(resulTasks.Count);
+        Console.WriteLine(numbers.Count);
+    }
+
+}//ConcurrentBag/ConcurrentDictionary Collections for async functions
